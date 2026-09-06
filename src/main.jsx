4bed5460b08d7fb55eee658293e60c3e92db7839
yatraflowos-gif <yatraflowos@gmail.com>
2026-09-06 11:57:56 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
+import { DataProvider } from './context/DataContext.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <div
-    style={{
-      minHeight: '100vh',
-      background: '#05070d',
-      color: '#ffffff',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      fontSize: '32px',
-      fontFamily: 'Arial, sans-serif',
-    }}
-  >
-    YatraFlow TEST 🚀
-  </div>
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <DataProvider>
+          <App />
+        </DataProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
 )
